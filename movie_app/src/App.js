20436de43c2862f9235679f22b5dbd415564f188
JiWon <radiohead@knou.ac.kr>
2022-06-29 16:10:18 +0900

    Update App.js

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,39 +1,8 @@
 import React from "react";
 
 class App extends React.Component {
-    // state 객체
-    state = {
-        count: 0,
-    };
-    // state 변경 시 setState 사용
-    // 현재 state를 current 인자로 받아서 변경
-    add = () => {
-        this.setState(current => 
-            ({ count: this.state.count + 1})
-            );
-    };
-
-    minus = () => { 
-        this.setState(current => (
-            { count: this.state.count - 1 }
-            )
-         );
-    };
-    
-    // jsx 반환용 render
     render() {
-        console.log('render');
-        return (
-        <div>
-            <h1>number is: {this.state.count}</h1>
-            <button onClick={this.add}>Add</button>
-            <button onClick={this.minus}>Minus</button>
-        </div>);
-    }
-       
-    constructor(props) {
-        super(props); 
-        console.log('hello');
+        return <div />;
     }
 }
 
