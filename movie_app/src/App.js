63303cf60dce03fa3eea9e1ba792994305f8e3a2
JiWon <radiohead@knou.ac.kr>
2022-06-30 14:18:56 +0900

    api 호출
    import axios
    state 추가
    getMovies 함수 키워드 포함 작성
    componentDidMount()로딩시 getMovies 실행

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
+import axios from 'axios';
 
 class App extends React.Component {
+    state = {
+        isLoading: true,
+        movies: [],
+    };
+    // api 실행 분리
+    getMovies = async () => { // 키워드 asyns 함수실행시 시간 소요(비동기) / await 실제 시간 필요 대상
+        const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    }
+
+    componentDidMount(){ //영화 데이터 로딩
+        this.getMovies();
+    }
+
     render() {
-        return <div />;
+        const { isLoading } = this.state;
+        return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;;
     }
 }
 
