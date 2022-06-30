3a3b92873d6ec4a4ef5b1595c846fd23d710f8be
JiWon <radiohead@knou.ac.kr>
2022-06-30 16:09:05 +0900

    포스터, 내용 출력
    포스터, 내용, 제목등 출력
    html 작성
    css 파일 생성

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from 'axios';
+import Movie from "./Movie";
+import './App.css';
 
 class App extends React.Component {
     state = {
@@ -12,7 +14,7 @@ class App extends React.Component {
             data:{
                 data:{ movies },
             },
-        } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+        } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
         this.setState({ movies, isLoading: false }); // axios get : state 이름 같아서 축약
     }
 
@@ -20,9 +22,30 @@ class App extends React.Component {
         this.getMovies();
     }
 
-    render() {
-        const { isLoading } = this.state;
-        return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;;
+    render() { //문법이 바꼈나 className 해야 정상동작
+        const { isLoading, movies } = this.state;
+        return ( 
+            <section className="container"> 
+            {isLoading ? (
+                <div className="loader">
+                    <span className="loader__text">Loading...</span>
+                </div> 
+                ) : (
+                <div className="movies">    
+                   { movies.map(movie => ( 
+                    <Movie
+                    key={movie.id}
+                    id={movie.id}
+                    year={movie.year}
+                    title={movie.title}
+                    summary={movie.summary}
+                    poster={movie.medium_cover_image}
+                    />
+                ))}; //컴포넌트 출력
+            </div>
+        )}
+        </section>
+        );
     }
 }
 
