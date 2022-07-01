87f8336174c9aa401f1118106825a3e6e20b0d02
JiWon <radiohead@knou.ac.kr>
2022-07-01 15:17:28 +0900

    라우터 사용
    에러나서 react-router-dom 다운그레이드함
    Home은 잘나오는데 about안나옴 왜지

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import Home from "./routes/Home";
 import './App.css';
+import { HashRouter, Route } from 'react-router-dom'; //라우터 url, 컴포넌트 매칭
+import About from './routes/About';
 
 function App() {
-    return <Home />;
+    return (
+        <HashRouter>
+            <Route path="/" component={Home} />
+            <Route path="/about" component={About} />
+        </HashRouter>
+    );
 }
 
 export default App;
