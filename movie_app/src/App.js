56c4d4468e62b21f9a1eb1556def08eb70794f33
JiWon <radiohead@knou.ac.kr>
2022-07-04 10:57:21 +0900

    HashRouter 적용
    HashRouter로 exact={true} 해당 링크에만 작동
    Link to로 변하는 부분만 그리기
    근데 네비게이션 누르명 주소만 변하고 페이지 랜딩안됨..새로고침 해야댐

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -3,11 +3,13 @@ import Home from "./routes/Home";
 import './App.css';
 import { HashRouter, Route } from 'react-router-dom'; //라우터 url, 컴포넌트 매칭
 import About from './routes/About';
+import Navigation from "./components/Navigation";
 
 function App() {
     return (
         <HashRouter>
-            <Route path="/" component={Home} />
+            <Navigation />
+            <Route path="/" exact={true} component={Home} />
             <Route path="/about" component={About} />
         </HashRouter>
     );
