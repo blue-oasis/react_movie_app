38d6319dd0983ec2a514e57c9773597a441db575
JiWon <radiohead@knou.ac.kr>
2022-07-04 14:13:27 +0900

    네비게이션 추가
    네비게이션 스타일 적용

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import { HashRouter, Route } from 'react-router-dom'; //라우터 url, 컴포넌트 매칭
 import About from './routes/About';
 import Navigation from "./components/Navigation";
+import Detail from "./routes/Detail";
 
 function App() {
     return (
         <HashRouter>
             <Navigation />
-            <Route path="/" exact={true} component={Home} />
-            <Route path="/about" component={About} />
+            <Route path="/" exact={true} component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/movie-detail" component={Detail} />
         </HashRouter>
     );
 }
